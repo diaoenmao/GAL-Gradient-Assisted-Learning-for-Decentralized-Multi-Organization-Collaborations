3e3dfe4641985c61bd31b97a32c4e0f9bcf7739f
dem123456789 <diao_em@hotmail.com>
2021-03-07 00:45:53 -0500

    update

--- src/make_stats.py
+++ src/make_stats.py
@@ -1,7 +1,8 @@
 import os
+import torch
 from config import cfg
 from data import fetch_dataset, make_data_loader
-from utils import save, collate, Stats, makedir_exist_ok, process_control
+from utils import save, collate, Stats, makedir_exist_ok, process_control, process_dataset
 
 if __name__ == "__main__":
     stats_path = './res/stats'
@@ -10,15 +11,16 @@ if __name__ == "__main__":
                   'CIFAR10']
     process_control()
     cfg['seed'] = 0
-    cfg['tag'] = {'batch_size': {'train': 128, 'test': 128}}
-    for data_name in data_names:
-        dataset = fetch_dataset(data_name)
-        data_loader = make_data_loader(dataset, cfg['model_name'])
-        stats = Stats(dim=dim)
-        for i, input in enumerate(data_loader['train']):
-            input = collate(input)
-            stats.update(input['data'])
-        stats = (stats.mean.tolist(), stats.std.tolist())
-        print(data_name, stats)
-        makedir_exist_ok(stats_path)
-        save(stats, os.path.join(stats_path, '{}.pt'.format(data_name)))
+    with torch.no_grad():
+        for data_name in data_names:
+            dataset = fetch_dataset(data_name)
+            process_dataset(dataset)
+            data_loader = make_data_loader(dataset, cfg['model_name'])
+            stats = Stats(dim=dim)
+            for i, input in enumerate(data_loader['train']):
+                input = collate(input)
+                stats.update(input['data'])
+            stats = (stats.mean.tolist(), stats.std.tolist())
+            print(data_name, stats)
+            makedir_exist_ok(stats_path)
+            save(stats, os.path.join(stats_path, '{}.pt'.format(data_name)))
